a3f6aa1e7634da8c58bd2274ba05655211ae62ed
Florian86 <florian86@gmail.com>
2015-01-02 11:13:16 +0100

    Erste, noch nicht funktionsfähige, Version von M2T

--- src/de/tu_bs/cs/isf/mbse/ernaehrungsplan/HTMLGenerator.java
+++ src/de/tu_bs/cs/isf/mbse/ernaehrungsplan/HTMLGenerator.java
@@ -1,10 +1,19 @@
 package de.tu_bs.cs.isf.mbse.ernaehrungsplan;
 
+import java.io.IOException;
+
+import de.tu_bs.cs.isf.ecore.util.XMIUtil;
+
 public class HTMLGenerator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		/*
+		 * Es gibt die Möglichkeit mit Java sich die Ausgabe generieren zu lassen.
+		 * Oder z.B. mit Xpand.
+		 * 
+		 * Hier ist nur die erste Zeile für die Generierung mit Java
+		 */
+		Ernaehrungsplan ernaehrungsplan = XMIUtil.load("model/Ernaehrungsplan.xmi", ErnaehrungsplanPackage.eINSTANCE, Ernaehrungsplan.class);
 	}
 
 }
